09eabc10ce69fe6535a5a83d4655f146931eac89
NL <nolann.laine@creatis.insa-lyon.fr>
2021-11-25 12:23:03 +0100

    comment

--- SEGMENTATION/classes/datasetBuilderCUBS.py
+++ SEGMENTATION/classes/datasetBuilderCUBS.py
@@ -29,8 +29,8 @@ class datasetBuilder():
         for data_base in self.p.DATABASE_NAME:
             files_cubs = get_files(self.p.PATH_TO_SEQUENCES) # name of all images in self.p.PATH_TO_SEQUENCES
             # --- loop over all the images in the database
-            for img_index in tqdm(range(len(files_cubs)), ascii=True, desc='Patch (Mask + image): ' + data_base):
             # for img_index in tqdm(range(len(files_cubs)), ascii=True, desc='Patch (Mask + image): ' + data_base):
+            for img_index in tqdm(range(len(files_cubs[0:100])), ascii=True, desc='Patch (Mask + image): ' + data_base):
                 # --- load the image and the calibration factor
                 spatial_res_y, img_f = load_tiff(os.path.join(self.p.PATH_TO_SEQUENCES, files_cubs[img_index]), PATH_TO_CF=self.p.PATH_TO_CF)
                 spatial_resolution_x = spatial_res_y
@@ -69,7 +69,7 @@ class datasetBuilder():
         ''' Saves patches in .h5 file with train/validation/test sets. '''
         f = h5py.File(path, "w")
         for key in self.dic_datas.keys():
-            print(f"the training set {key} is being written.")
+            print(f"The {key} set is being written.")
 
             gr = key
             tmpGr = f.create_group(name=gr)
