712fda7d50add51d2052ea1c5ded3c04b25b89d8
Nolann Lainé <60877526+nl3769@users.noreply.github.com>
2021-11-25 08:57:56 +0100

    comment save_references.m

--- CREATE_REFERENCES_CUBS/save_references.m
+++ CREATE_REFERENCES_CUBS/save_references.m
@@ -1,18 +1,17 @@
 function[] = save_references(LI, MA, path, name, expert, image)
+    % Saves the segmentation in .mat file
     
-    % --- we save the segmentation    
     widthImage=size(image, 2);
     segLI=zeros(widthImage, 1);
     segLI(LI(:,1))=LI(:,2);
     
     segMA=zeros(widthImage, 1);
     segMA(MA(:,1))=MA(:, 2);
-
-    % --- we first save the LI interface
+    % --- saves the LI interface
     seg=segLI;
     pathToSaveSegLI=strcat(path, 'CONTOURS/', expert, '/', name, '_IFC3_A1', '.mat');
     save(pathToSaveSegLI, 'seg');
-    
+    % --- saves the MA interface
     seg = segMA ;
     pathToSaveSegMA=strcat(path, 'CONTOURS/', expert, '/', name, '_IFC4_A1', '.mat');
     save(pathToSaveSegMA, 'seg');
