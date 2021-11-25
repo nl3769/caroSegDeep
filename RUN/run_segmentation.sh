21f6a2c1850013fd3f4b66f090837280e232a068
NL <nolann.laine@creatis.insa-lyon.fr>
2021-11-25 13:25:17 +0100

    shell

---
+++ RUN/run_segmentation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate creatis
+
+working_directory=~/Documents/REPO/caroSegDeep/SEGMENTATION
+cd $working_directory
+PYTHONPATH=$working_directory python scripts/run_segmentation.py -param set_parameters_inference_template.py
