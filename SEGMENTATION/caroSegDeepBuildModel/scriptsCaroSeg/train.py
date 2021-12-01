964a8829d3d19c42ca86e048c7e6f7576fb48c38
Nolann Lainé <60877526+nl3769@users.noreply.github.com>
2021-12-01 10:57:25 +0100

    Update train.py

--- SEGMENTATION/caroSegDeepBuildModel/scriptsCaroSeg/train.py
+++ SEGMENTATION/caroSegDeepBuildModel/scriptsCaroSeg/train.py
@@ -17,7 +17,7 @@ import datetime
 
 
 from caroSegDeepBuildModel.KerasSegmentationFunctions.metrics import iou, iou_thresholded, dice_coef
-from caroSegDeepBuildModel.KerasSegmentationFunctions.losses import binary_cross_entropy, dice_bce_constraint_MAE, dice_bce_constraint_thickness
+from caroSegDeepBuildModel.KerasSegmentationFunctions.losses import dice_bce_loss, dice_bce_constraint_MAE, dice_bce_constraint_thickness
 
 from caroSegDeepBuildModel.functionsCaroSeg.save_history import save_IOU, save_loss, save_DICE
 
@@ -30,27 +30,19 @@ from caroSegDeepBuildModel.functionsCaroSeg.model_selection import *
 
 # ----------------------------------------------------------------
 def train(p):
-
     ''' Trains the model from scratch. Training parameters, etc., are defined in set_parameters_training_*.py. '''
-
     # --- get dataset
     data = h5py.File(p.PATH_TO_DATASET, 'r')
-    # --- get the dimension of an images for parameters
+    # --- get the dimension of an image
     dim_img = data["train"]["img"][list(data["train"]["img"].keys())[0]][()].shape
     # --- parameters for training generator
     params_training = {'dim': dim_img + (1,), 'batch_size': p.BATCH_SIZE, 'shuffle': True}
     # --- parameters for validation generator
     params_val = {'dim': dim_img + (1,), 'batch_size': 1, 'shuffle': False}
     # --- training generator
-    training_generator = DataGenerator1Channel(partitions = data["train"]["img"],
-                                              labels = data["train"]["masks"],
-                                              data_augmentation = p.DATA_AUGMENTATION,
-                                              **params_training)
+    training_generator = DataGenerator1Channel(partitions = data["train"]["img"], labels = data["train"]["masks"],  data_augmentation = p.DATA_AUGMENTATION, **params_training)
     # --- validation generator
-    validation_generator = DataGenerator1Channel(partitions = data["validation"]["img"],
-                                                labels = data["validation"]["masks"],
-                                                data_augmentation=False,
-                                                **params_val)
+    validation_generator = DataGenerator1Channel(partitions = data["validation"]["img"], labels = data["validation"]["masks"], data_augmentation=False, **params_val)
     # --- path where the results will be saved: figures + model + metrics
     chekDir(os.path.join(p.PATH_TO_SAVE_RESULTS_PDF_METRICS_WEIGHTS, p.NAME_OF_THE_EXPERIMENT))
     model = model_selection(model_name = p.MODEL_SELECTION, input_shape = dim_img + (1,), patch_width = p.PATCH_WIDTH)
