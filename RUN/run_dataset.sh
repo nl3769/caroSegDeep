af1dfd096a24bbe709f932779517d9a5a9c778ea
Nolann Laine <nolann.laine@creatis.insa-lyon.fr>
2022-07-18 14:12:43 +0200

    change shell

--- RUN/run_dataset.sh
+++ RUN/run_dataset.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-#PBS -l walltime=1:00:00
-#PBS -l nodes=1:ppn=1
-#PBS -l mem=90GB
-#PBS -m ae
-#PBS -e dataset_gen.err
-#PBS -o dataset_gen.out
-#PBS -N dataset_gen
-#PBS -M laine@creatis.insa-lyon.fr
 
-
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate caroSegDeep
-
-working_directory=/home/laine/REPOSITORIES/caroSegDeep/SEGMENTATION
-cd $working_directory
-PYTHONPATH=$working_directory python scripts/run_dataset.py -param set_parameters_dataset_template.py
+WD=/home/laine/REPOSITORIES/caroSegDeep/SEGMENTATION
+cd $WD
+PYTHONPATH=$WD python run/run_dataset.py -param set_parameters_dataset_template.py
--- RUN/run_segmentation.sh
+++ RUN/run_segmentation.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate creatis
 
-working_directory=~/Documents/REPO/caroSegDeep/SEGMENTATION
-
-cd $working_directory
-PYTHONPATH=$working_directory python scripts/run_IMC_segmentation.py -param set_parameters_inference_template.py
+WD=~/Documents/REPO/caroSegDeep/SEGMENTATION
+cd $WD
+PYTHONPATH=$WD python scripts/run_IMC_segmentation.py -param set_parameters_inference_template.py
