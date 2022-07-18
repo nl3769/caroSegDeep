6a1523032f58d362607de209a2ce75f13ecdce87
Nolann Laine <nolann.laine@creatis.insa-lyon.fr>
2022-07-18 10:53:05 +0200

    update parameters

--- SEGMENTATION/package_parameters/parameters_far_wall_deep_training.py
+++ SEGMENTATION/package_parameters/parameters_far_wall_deep_training.py
@@ -15,21 +15,23 @@ class Parameters:
           PATH_TO_SAVE_TENSORBOARD,
           PATH_TO_SAVE_RESULTS_PDF_METRICS_WEIGHTS,
           NAME_OF_THE_EXPERIMENT,
+          PATH_PRETRAINED_MODEL,
           PATH_TO_DATASET,
           PATH_FOLD
   ):
-    self.NB_EPOCH = NB_EPOCH
-    self.NBPATIENCE_EPOCHS = NBPATIENCE_EPOCHS
-    self.BATCH_SIZE = BATCH_SIZE
-    self.DATA_AUGMENTATION = DATA_AUGMENTATION
-    self.MODEL_SELECTION = MODEL_SELECTION
-    self.LEARNING_RATE = LEARNING_RATE
-    self.LOSS = LOSS
-    self.PATH_TO_SAVE_PREDICTION_DURING_TRAINING = PATH_TO_SAVE_PREDICTION_DURING_TRAINING
-    self.PATCH_HEIGHT = PATCH_HEIGHT
-    self.PATCH_WIDTH = PATCH_WIDTH
-    self.PATH_TO_SAVE_TENSORBOARD = PATH_TO_SAVE_TENSORBOARD
-    self.PATH_TO_SAVE_RESULTS_PDF_METRICS_WEIGHTS = PATH_TO_SAVE_RESULTS_PDF_METRICS_WEIGHTS
-    self. NAME_OF_THE_EXPERIMENT = NAME_OF_THE_EXPERIMENT
-    self.PATH_TO_DATASET = PATH_TO_DATASET
-    self.PATH_FOLD = PATH_FOLD
+    self.NB_EPOCH                                     = NB_EPOCH
+    self.NBPATIENCE_EPOCHS                            = NBPATIENCE_EPOCHS
+    self.BATCH_SIZE                                   = BATCH_SIZE
+    self.DATA_AUGMENTATION                            = DATA_AUGMENTATION
+    self.MODEL_SELECTION                              = MODEL_SELECTION
+    self.LEARNING_RATE                                = LEARNING_RATE
+    self.LOSS                                         = LOSS
+    self.PATH_TO_SAVE_PREDICTION_DURING_TRAINING      = PATH_TO_SAVE_PREDICTION_DURING_TRAINING
+    self.PATCH_HEIGHT                                 = PATCH_HEIGHT
+    self.PATCH_WIDTH                                  = PATCH_WIDTH
+    self.PATH_TO_SAVE_TENSORBOARD                     = PATH_TO_SAVE_TENSORBOARD
+    self.PATH_TO_SAVE_RESULTS_PDF_METRICS_WEIGHTS     = PATH_TO_SAVE_RESULTS_PDF_METRICS_WEIGHTS
+    self. NAME_OF_THE_EXPERIMENT                      = NAME_OF_THE_EXPERIMENT
+    self.PATH_PRETRAINED_MODEL                        = PATH_PRETRAINED_MODEL
+    self.PATH_TO_DATASET                              = PATH_TO_DATASET
+    self.PATH_FOLD                                    = PATH_FOLD
